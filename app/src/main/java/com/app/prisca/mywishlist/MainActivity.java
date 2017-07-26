ba02097171cc56f73d43a16bedadbef5fc982267
priscarelique <prisca.relique@gmail.com>
2017-07-26 20:49:37 +0200

    add home view

--- app/src/main/java/com/app/prisca/mywishlist/MainActivity.java
+++ app/src/main/java/com/app/prisca/mywishlist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.app.prisca.mywishlist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+    public void signIn(View view) {
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
+    }
+
 }
